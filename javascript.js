8add88560ed1ec0dc4df5b53830e13ac8a56cd15
AndreaZenz <zenzero.ere@gmail.com>
2021-04-07 16:26:42 +0200

    finish

---
+++ javascript.js
@@ -0,0 +1,6 @@
+var nome= prompt("quale è il tuo nome?")
+var cognome= prompt("Qual'è il tuo cognome?")
+var colore= prompt(nome + " " + cognome + " " + "Qual'è il tuo colore preferito?")
+document.getElementById("utente").innerHTML =
+"Ciao " + nome + " " + cognome + " ero sicuro che il tuo colore preferito fosse il " + colore + " 21"
+
